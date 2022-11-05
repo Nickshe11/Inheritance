4623d537048465af06d1c8d24f114c5988d17c57
Nickshe11 <shelobod@gmail.com>
2022-11-05 20:36:35 +0300

    Add project files.

---
+++ Academy/Main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+	std::string last_name;
+	std::string first_name;
+	unsigned int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	
+	//Constructors
+	Human(const std::string& last_name, const std::string& first_name, unsigned int age):
+		last_name(last_name), first_name(first_name), age(age)
+	{
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+	//Methods
+	void print()const
+	{
+		cout << last_name << " " << first_name << " " << age << " лет.\n";
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Human human("Montana", "Antonio", 25);
+	human.print();
+}
